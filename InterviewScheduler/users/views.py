ef124450035ecee4a508ce816dd291d66a52890b
Richesh Gupta <rgrichesh45@gmail.com>
2021-11-06 09:37:27 +0530

    Fix: Interview Add

--- InterviewScheduler/users/views.py
+++ InterviewScheduler/users/views.py
@@ -15,7 +15,6 @@ def ErrorPage(request,msg):
     return render(request,"users/error.html",{'msg':msg})
 
 def InterviewPage(request):
-    # user_obj = Participant.objects.all()
     users = User.objects.filter(is_staff=False,is_active=True)
     context = {'users':users}
     return render(request,"users/interviewpage.html",context)
@@ -28,36 +27,28 @@ class TestUI(CreateView):
         return super().form_valid(InterviewForm)
 
 
-def IsValidInterview(request):
-    if(request.method=="GET"):
-        return False
-    else:
-        return True
-
 
 def GetOrCreateInterview(request):
     if request.method=="POST":
         from_date = request.POST.getlist('from_date')
         to_date = request.POST.getlist('to_date')
-        # participants = request.POST.getlist('participants')
+        participants = request.POST.getlist('participants')
+
         if(len(participants)<2):
             return ErrorPage(request,"Less than 2 people can't create interview room")
-
-
+        
         from_date = from_date[0]
         to_date = to_date[0]
         from_date = datetime.datetime.strptime(from_date, '%Y-%m-%d %H:%M:%S')
         to_date = datetime.datetime.strptime(to_date, '%Y-%m-%d %H:%M:%S')
-
-        print(from_date)
+        from_date = make_aware(from_date)
+        to_date = make_aware(to_date)
+        print("from date ",from_date)
         
-        try:
-            objs  = Interview.objects.filter(start_time = from_date,end_time = to_date)
-        except:
+        objs  = Interview.objects.filter(start_time = from_date,end_time = to_date)
+        if(not objs):    
             objs = Interview.objects.create(start_time=from_date,end_time = to_date,is_active = True)
             
-        
-        
     else:
         return render(request,"users/interviewpage.html",{})
     return render(request,"users/interviewpage.html",{})
